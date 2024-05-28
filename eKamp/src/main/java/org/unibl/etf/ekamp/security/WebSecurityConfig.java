058313c784a11dbe40bc053def7010a5dfceb788
Pavle Colic <pavle99colic@gmail.com>
2024-05-28 22:08:25 +0200

    Security folder

--- eKamp/src/main/java/org/unibl/etf/ekamp/security/WebSecurityConfig.java
+++ eKamp/src/main/java/org/unibl/etf/ekamp/security/WebSecurityConfig.java
@@ -1,12 +1,28 @@
 package org.unibl.etf.ekamp.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.config.core.GrantedAuthorityDefaults;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.unibl.etf.ekamp.security.model.AuthorizationRules;
+import org.unibl.etf.ekamp.security.model.Rule;
 import org.unibl.etf.ekamp.services.JwtEmployeeDetailsService;
-import org.unibl.etf.ekamp.security.AuthorizationFilter;
+
+import java.io.IOException;
+
 @EnableWebSecurity
 @Configuration
 public class WebSecurityConfig {
@@ -24,4 +40,44 @@ public class WebSecurityConfig {
         return authenticationConfiguration.getAuthenticationManager();
     }
 
+    @Bean
+    protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        return http.cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(c -> c.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(this::createRules)
+                .userDetailsService(jwtEmployeeDetailsService)
+                .addFilterBefore(authorizationFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    private void createRules(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry interceptor) {
+        AuthorizationRules authorizationRules;
+        try {
+            authorizationRules = new ObjectMapper().readValue(new ClassPathResource("rules.json").getInputStream(), AuthorizationRules.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        interceptor.requestMatchers(HttpMethod.POST, "/login").permitAll();
+        interceptor.requestMatchers(HttpMethod.POST, "/sign-up").permitAll();
+        for (Rule rule : authorizationRules.getRules()) {
+            if (rule.getMethods().isEmpty())
+                interceptor.requestMatchers(rule.getPattern()).hasAnyAuthority(rule.getRoles().toArray(String[]::new));
+            else for (String method : rule.getMethods()) {
+                interceptor.requestMatchers(HttpMethod.valueOf(method), rule.getPattern()).hasAnyAuthority(rule.getRoles().toArray(String[]::new));
+            }
+        }
+        interceptor.anyRequest().denyAll();
+    }
+
+
+    @Bean
+    GrantedAuthorityDefaults grantedAuthorityDefaults() {
+        return new GrantedAuthorityDefaults(""); // Remove the ROLE_ prefix
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
--- eKamp/src/main/java/org/unibl/etf/ekamp/services/JwtEmployeeDetailsService.java
+++ eKamp/src/main/java/org/unibl/etf/ekamp/services/JwtEmployeeDetailsService.java
@@ -1,8 +1,9 @@
 package org.unibl.etf.ekamp.services;
 
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.unibl.etf.ekamp.model.dto.JwtEmployee;
 
-public interface JwtEmployeeDetailsService {
+public interface JwtEmployeeDetailsService extends UserDetailsService {
     JwtEmployee loadEmployeeByUsername(String username) throws UsernameNotFoundException;
 }
