839905b8bd673f44829e10bab77d91435fb4a6fd
DejanIkic <dikic19999@gmail.com>
2024-05-31 16:54:05 +0200

    polje u message klasei

--- eKamp/src/main/java/org/unibl/etf/ekamp/model/dto/Message.java
+++ eKamp/src/main/java/org/unibl/etf/ekamp/model/dto/Message.java
@@ -8,7 +8,7 @@ import java.time.LocalDateTime;
 @Data
 public class Message {
     private Integer id;
-    private String data;
+    private String content;
     private LocalDateTime creationTime;
     private String employeeUsername;
 }
