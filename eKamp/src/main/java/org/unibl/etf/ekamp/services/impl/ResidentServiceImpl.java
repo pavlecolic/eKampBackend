041989d662e31575fd2e8e048ffb10bc9997bdad
Pavle Colic <pavle99colic@gmail.com>
2024-06-06 16:01:35 +0200

    Dodaje se ResidencePeriod sa Residentom

--- eKamp/src/main/java/org/unibl/etf/ekamp/services/impl/ResidentServiceImpl.java
+++ eKamp/src/main/java/org/unibl/etf/ekamp/services/impl/ResidentServiceImpl.java
@@ -4,11 +4,20 @@ import jakarta.transaction.Transactional;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.unibl.etf.ekamp.base.CrudJpaService;
+import org.unibl.etf.ekamp.model.dto.Assignment;
+import org.unibl.etf.ekamp.model.dto.ResidencePeriod;
+import org.unibl.etf.ekamp.model.dto.Resident;
+import org.unibl.etf.ekamp.model.dto.SimpleResidencePeriod;
+import org.unibl.etf.ekamp.model.entities.AssignmentEntity;
 import org.unibl.etf.ekamp.model.entities.ResidencePeriodEntity;
 import org.unibl.etf.ekamp.model.entities.ResidentEntity;
 import org.unibl.etf.ekamp.model.requests.DepartureRequest;
+import org.unibl.etf.ekamp.model.requests.ResidentRequest;
+import org.unibl.etf.ekamp.repositories.EmployeeEntityRepository;
 import org.unibl.etf.ekamp.repositories.ResidencePeriodRepository;
 import org.unibl.etf.ekamp.repositories.ResidentEntityRepository;
+import org.unibl.etf.ekamp.services.EmployeeService;
+import org.unibl.etf.ekamp.services.ResidencePeriodService;
 import org.unibl.etf.ekamp.services.ResidentService;
 
 import java.sql.Timestamp;
@@ -20,16 +29,39 @@ import java.util.List;
 public class ResidentServiceImpl extends CrudJpaService<ResidentEntity, Integer> implements ResidentService {
 
     private final ResidencePeriodRepository residencePeriodRepository;
-
-    public ResidentServiceImpl(ResidentEntityRepository repository, ModelMapper modelMapper, ResidencePeriodRepository residencePeriodRepository) {
+    private final EmployeeEntityRepository employeeEntityRepository;
+    private final ResidencePeriodService residencePeriodService;
+    private final EmployeeService employeeService;
+    public ResidentServiceImpl(ResidentEntityRepository repository, ModelMapper modelMapper, ResidencePeriodRepository residencePeriodRepository, EmployeeEntityRepository employeeEntityRepository, ResidencePeriodService residencePeriodService, EmployeeService employeeService) {
         super(repository, ResidentEntity.class, modelMapper);
         this.residencePeriodRepository = residencePeriodRepository;
+        this.employeeEntityRepository = employeeEntityRepository;
+        this.residencePeriodService = residencePeriodService;
+        this.employeeService = employeeService;
     }
 
+    @Override
     public void depart(DepartureRequest request) {
         List<ResidencePeriodEntity> residencePeriodEntityList = residencePeriodRepository.findByResidentIdAndCampId(request.getResidentId(), request.getCampId());
         residencePeriodEntityList.stream()
                 .filter(rp -> rp.getEndDate() == null)
                 .forEach(rp -> rp.setEndDate(Timestamp.from(Instant.now())));
+    }
+
+    @Override
+    public Resident addResident(ResidentRequest request) {
+        ResidentEntity entity = getModelMapper().map(request, ResidentEntity.class);
+        entity.setId(null);
+        AssignmentEntity assignmentEntity = employeeEntityRepository.getCurrentAssignment(request.getEmployeeId());
+        SimpleResidencePeriod simpleResidencePeriod = new SimpleResidencePeriod();
+        simpleResidencePeriod.setCampId(assignmentEntity.getCamp().getId());
+        simpleResidencePeriod.setStartDate(Timestamp.from(Instant.now()));
+        simpleResidencePeriod.setEndDate(null);
+
+        entity = getRepository().saveAndFlush(entity);
+        getEntityManager().refresh(entity);
+        simpleResidencePeriod.setResidentId(entity.getId());
+        SimpleResidencePeriod srp = residencePeriodService.insert(getModelMapper().map(simpleResidencePeriod, ResidencePeriodEntity.class), SimpleResidencePeriod.class );
+        return getModelMapper().map(entity, Resident.class);
     }
 }
